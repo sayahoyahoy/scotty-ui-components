c7c4765d7073fa95a2ecd920ff0d3eb7d34cde64
Tom Hodbod <tom@sayahoyahoy.com>
2024-09-20 22:59:33 +0200

    radix

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
 import {classes} from './app.st.css'
 import BtnSample from './components/btn-sample/btn-sample.vue'
 import Container from './components/cmps-container/cmps-container.vue'
+import SwitchSample from './components/switch-sample/switch-sample.vue'
 </script>
 
 <template>
     <div :class="classes.app">
-        <container label="Test Btns">
+        <container label="Test">
             <btn-sample :black="true">
                 Ahoy
             </btn-sample>
             <btn-sample size="large">
                 Ahoy
             </btn-sample>
+            <switch-sample />
         </container>
     </div>
 </template>
---
+++ src/components/switch-sample/switch-sample.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {SwitchRoot, SwitchThumb} from 'radix-vue'
+import {classes, st} from './switch-sample.st.css'
+</script>
+
+<template>
+    <div :style="{ display: 'flex', alignItems: 'center' }">
+        <label
+            :class="classes.label"
+            for="airplane-mode"
+        >
+            Airplane mode
+        </label>
+        <switch-root
+            id="airplane-mode"
+            :class="st(classes.root, {})"
+        >
+            <switch-thumb
+                :class="classes.thumb"
+            />
+        </switch-root>
+    </div>
+</template>
